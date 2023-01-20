928287e50dad717f94ae2d04580b67c3a34928f3
Mark <mark.aaron.fisher@gmail.com>
2023-01-20 14:50:09 -0800

    change the environment variables to be strings instead of JSON which did not seem valid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { IntlProvider } from "react-intl";
 import * as englishMessages from "../lang/en.json";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// import { firebaseConfig } from "../keys";
+import { firebaseConfig } from "../keys";
 
 export default function App({ Component, pageProps }: AppProps) {
   const messageMap = {
@@ -15,9 +15,40 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // const app = initializeApp(firebaseConfig);
 
-  const app = initializeApp(process?.env?.firebaseConfig);
-  if (app.name && typeof window !== "undefined") {
-    const analytics = getAnalytics(app);
+  // const configData: {
+  //   apiKey: string;
+  //   authDomain: string;
+  //   projectId: string;
+  //   storageBucket: string;
+  //   messagingSenderId: string;
+  //   appId: string;
+  //   measurementId: string;
+  // } = firebaseConfig;
+  const configData:
+    | {
+        apiKey: string;
+        authDomain: string;
+        projectId: string;
+        storageBucket: string;
+        messagingSenderId: string;
+        appId: string;
+        measurementId: string;
+      }
+    | undefined = {
+    apiKey: process?.env?.apiKey || "",
+    authDomain: process?.env?.authDomain || "",
+    projectId: process?.env?.projectId || "",
+    storageBucket: process?.env?.storageBucket || "",
+    messagingSenderId: process?.env?.messagingSenderId || "",
+    appId: process?.env?.appId || "",
+    measurementId: process?.env?.measurementId || "",
+  };
+  if (configData?.apiKey) {
+    // @TODO flesh out with a more robust checker of non-emptiness
+    const app = initializeApp(configData);
+    if (app.name && typeof window !== "undefined") {
+      const analytics = getAnalytics(app);
+    }
   }
 
   return (
