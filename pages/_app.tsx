625395d9163021da54c9b6429a926dcb5c2003c5
Mark <mark.aaron.fisher@gmail.com>
2023-01-20 14:30:12 -0800

    add an environment variable on vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { IntlProvider } from "react-intl";
 import * as englishMessages from "../lang/en.json";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { firebaseConfig } from "../keys";
+// import { firebaseConfig } from "../keys";
 
 export default function App({ Component, pageProps }: AppProps) {
   const messageMap = {
@@ -13,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
   };
   const locale = "en";
 
-  const app = initializeApp(firebaseConfig);
+  // const app = initializeApp(firebaseConfig);
+
+  const app = initializeApp(process?.env?.firebaseConfig);
   if (app.name && typeof window !== "undefined") {
     const analytics = getAnalytics(app);
   }
