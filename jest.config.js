cf57931edf9bcd14ed4d3278acd00279e08ba407
Mark <mark.aaron.fisher@gmail.com>
2023-01-27 09:39:00 -0800

    add another possible jest.config and possibly rule it out

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   globals: {
+//     // we must specify a custom tsconfig for tests because we need the typescript transform
+//     // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+//     // can see this setting in tsconfig.jest.json -> "jsx": "react"
+//     "ts-jest": {
+//       tsConfig: "tsconfig.jest.json",
+//     },
+//   },
+// };
+
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
