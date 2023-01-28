4b973fba05c20976b9297322853f1c603262ce25
Mark <mark.aaron.fisher@gmail.com>
2023-01-27 19:31:23 -0800

    fix what seems likely to be a mis-commit

--- firebase.ts
+++ firebase.ts
@@ -7,29 +7,29 @@
 
 // Local
 
-import { secrets } from "./keys";
+// import { secrets } from "./keys";
 
-export const firebaseConfig = {
-  apiKey: secrets?.apiKey || "",
-  authDomain: secrets?.authDomain || "",
-  projectId: secrets?.projectId || "",
-  storageBucket: secrets?.storageBucket || "",
-  messagingSenderId: secrets?.messagingSenderId || "",
-  appId: secrets?.appId || "",
-  measurementId: secrets?.measurementId || "",
-};
+// export const firebaseConfig = {
+//   apiKey: secrets?.apiKey || "",
+//   authDomain: secrets?.authDomain || "",
+//   projectId: secrets?.projectId || "",
+//   storageBucket: secrets?.storageBucket || "",
+//   messagingSenderId: secrets?.messagingSenderId || "",
+//   appId: secrets?.appId || "",
+//   measurementId: secrets?.measurementId || "",
+// };
 
 // Remote
 
-// export const firebaseConfig = {
-//   apiKey: process.env.apiKey,
-//   authDomain: process.env.authDomain,
-//   projectId: process.env.projectId,
-//   storageBucket: process.env.storageBucket,
-//   messagingSenderId: process.env.messageSenderId,
-//   appId: process.env.appId,
-//   measurementId: process.env.measurementId,
-// };
+export const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messageSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
 
 //
 
