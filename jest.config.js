1304627460b90f340dda7263b23c7cbdf98a5514
Mark <mark.aaron.fisher@gmail.com>
2023-02-01 11:20:18 -0800

    get tests to pass again

--- jest.config.js
+++ jest.config.js
@@ -42,4 +42,8 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+// module.exports = createJestConfig(customJestConfig);
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ["/node_modules/(?!(firebase|@firebase)/)"],
+});
