ae57cec35ecc3c535f8f5b3d459fab759e4277c6
Mark <mark.aaron.fisher@gmail.com>
2023-03-17 17:03:23 -0700

    add tooltips to action buttons

--- components/EditActionButton/index.tsx
+++ components/EditActionButton/index.tsx
@@ -1,9 +1,18 @@
+import { Tooltip } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+
+import { useIntl, IntlShape } from "react-intl";
+
 const EditActionButton: React.FC<{ props: { id: string | number } }> = (
   props
 ) => {
+  const intl: IntlShape = useIntl();
   // @TODO add button click handler and possibly prevent propagation
-  return <EditIcon />;
+  return (
+    <Tooltip title={intl.formatMessage({ id: "EDIT", defaultMessage: "Edit" })}>
+      <EditIcon />
+    </Tooltip>
+  );
 };
 
 export default EditActionButton;
--- components/ViewActionButton/index.tsx
+++ components/ViewActionButton/index.tsx
@@ -1,12 +1,21 @@
+import { Tooltip } from "@mui/material";
 import ViewIcon from "@mui/icons-material/Launch";
+
+import { useIntl, IntlShape } from "react-intl";
+
 const ViewActionButton: React.FC<{ props: { id: string | number } }> = (
   props
 ) => {
+  const intl: IntlShape = useIntl();
   // @TODO add button click handler and possibly prevent propagation
   const handleViewClick = async () => {
     // console.log("deleteMe got here handleViewClick entered");
   };
-  return <ViewIcon onClick={handleViewClick} />;
+  return (
+    <Tooltip title={intl.formatMessage({ id: "VIEW", defaultMessage: "View" })}>
+      <ViewIcon onClick={handleViewClick} />
+    </Tooltip>
+  );
 };
 
 export default ViewActionButton;
